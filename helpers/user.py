e123df38f4cd487f2bca313db49c04a6cfe0054a
jacevedo7 <jacevedo@playvox.com>
2022-04-30 06:33:33 -0500

    Validate users and sign in

--- helpers/user.py
+++ helpers/user.py
@@ -1,7 +1,15 @@
 import re
+import db
 
 def phone_validation(phone_number):
     if( re.match('^([0-9]{10})|(123)$', phone_number) ):
+        return True
+    
+    return False
+
+def validate_user(user, password):
+    print(user)
+    if user["password"] == password:
         return True
     
     return False
