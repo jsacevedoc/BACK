9765b41186ecdf945a7631190e1e465a6a08c725
jacevedo7 <jacevedo@playvox.com>
2022-04-30 08:54:53 -0500

    Back end cleaning

--- app.py
+++ app.py
@@ -14,51 +14,6 @@ PORT = 3200
 
 # Get Methods 
 
-@app.route("/")
-def home():
-   print("Users count")
-   print(count_users())
-   print("All users")
-   print(get_all_users())
-   return "<h1 style='color:red'>This is home!</h1>"
-
-@app.route('/temp')
-def hello_world():
-    return render_template('index.html')
-
-@app.route("/qstr")
-def qs():
-    if request.args:
-        req = request.args
-        res = {}
-        for key, value in req.items():
-            res[key] = value
-        res = make_response(jsonify(res), 200)
-        return res
-
-    res = make_response(jsonify({"error": "No Query String"}), 404)
-    return res
-
-@app.route("/json")
-def get_json():
-    res = make_response(jsonify(INFO), 200)
-    return res
-
-@app.route("/json/<collection>/<member>") 
-def get_data(collection, member):
-    print("getting the value of %s in the collection %s"%(member,collection))
-    if collection in INFO:
-        member = INFO[collection].get(member)
-        if member:
-            res = make_response(jsonify({"res":member}), 200)
-            return res
-
-        res = make_response(jsonify({"error": "Not found"}), 404)
-        return res
-
-    res = make_response(jsonify({"error": "Not found"}), 404)
-    return res
-
 @app.route("/users")  
 def get_users():
     users_list = get_all_users()
@@ -106,20 +61,6 @@ def sign_in(username, password):
 
 # Post Methods
 
-@app.route("/json/<collection>", methods=["POST"])
-def create_col(collection):
-
-    req = request.get_json()
-
-    if collection in INFO:
-        res = make_response(jsonify({"error": "Collection already exists"}), 400)
-        return res
-
-    INFO.update({collection: req})
-
-    res = make_response(jsonify({"message": "Collection created"}), 201)
-    return res
-
 @app.route("/email/<email>", methods=["GET", "POST"])
 def email_verification(email):
     if re.match('^[(a-z0-9\_\-\.)]+@[(a-z0-9\_\-\.)]+\.[(a-z)]{2,15}$',email.lower()):
@@ -154,21 +95,3 @@ def update_phone(username):
         res = make_response(jsonify({}), 404)
 
     return res
-
-
-# Delete Method
-
-@app.route("/json/<collection>", methods=["DELETE"])
-def delete_col(collection):
-
-    if collection in INFO:
-        del INFO[collection]
-        res = make_response(jsonify(INFO), 200)
-        return res
-
-    res = make_response(jsonify({"error": "Collection not found"}), 404)
-    return res
-
-if __name__ == "__main__":
-    print("Server running in port %s"%(PORT))
-    app.run(host='0.0.0.0', port=PORT)
